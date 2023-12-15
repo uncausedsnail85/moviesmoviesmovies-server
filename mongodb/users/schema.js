6fb2e616809a75f53884e2ce42f424b12f57f965
Bryan Ang <bryan.ang>
2023-12-15 16:52:56 +0800

    change user role enum

--- mongodb/users/schema.js
+++ mongodb/users/schema.js
@@ -6,7 +6,7 @@ const schema = new mongoose.Schema ({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
-    role: { type: String, enum: ['ADMIN', 'USER', 'SUPER'], default: 'USER'},
+    role: { type: String, enum: ['ADMIN', 'USER', 'MOD'], default: 'USER'},
 
 },
 
